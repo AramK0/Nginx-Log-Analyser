57a79a2ba48d0fd0bf066345e0ab915f2667bd9b
‘AramK0’ <aramk6827@gmail.com>
2025-08-01 22:59:28 +0300

    added log analysys script

---
+++ log_analyser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+log_file="/var/log/nginx/access.log"
+
+echo "Top 5 ip addresses with most requests"
+awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "\nTop 5 most requested paths"
+
+awk '{print $7}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "\n Top 5 response status codes: "
+
+awk '{print $9}' "$log_file" | sort | uniq -c | sort -nr | head -5
+
+echo -e "Top 5 user agents: "
+
+awk -F\" '{print $6}' "$log_file" | sort | uniq -c | sort -nr | head -5
